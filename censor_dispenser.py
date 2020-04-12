26a4cf637d6aaf5edb9ea791e8bb937762877329
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-04-12 14:45:33 +1200

    Initial commit!

---
+++ censor_dispenser.py
@@ -0,0 +1,9 @@
+#author Chris Pidgeon
+#This is my attempt at the Censor Dispenser challenge project on the Computer Science path of Codecademy.
+
+#This challenge is focused around creating functions to interact with a few large strings (emails).
+#These emails must be censored according to multiple formats, lists of phrases to be censored, etc.
+email_one = open("email_one.txt", "r").read()
+email_two = open("email_two.txt", "r").read()
+email_three = open("email_three.txt", "r").read()
+email_four = open("email_four.txt", "r").read()
