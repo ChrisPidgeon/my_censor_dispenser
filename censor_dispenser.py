03b8cc623a431a1b33e6447d7916da3e9fa0c763
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-04-12 15:05:06 +1200

    Defined each exercise for future use.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -7,3 +7,15 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+def exercise_one():
+    pass
+
+def exercise_two():
+    pass
+
+def exercise_three():
+    pass
+
+def exercise_four():
+    pass
