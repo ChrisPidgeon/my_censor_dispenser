83eccd230a37884a9009c040681fa5608532b39a
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-04-12 20:02:42 +1200

    Added the official solution from Codecademy to the fourth exercise. This one proved troublesome for me! Better luck in the next challenge.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -9,6 +9,7 @@ email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressing", "concerning", "horrible", "horribly", "questionable"]
+punctuation = [",", "!", "?", ".", "%", "/", "(", ")"]
 
 #This exercise required me to define a function that can censor a specific word or 
 #phrase from a body of text, and then return the text.
@@ -53,10 +54,47 @@ def censor_phrase_list_and_negative_words(text):
     text_clean = censor_phrase_list(text_clean)
     return text_clean
 
-def exercise_four():
-    pass
+def censor_all_plus_before_after(text):
+    list_of_censors = proprietary_terms + negative_words
+    words_in_text = []
+    for item in text.split(" "):
+        split_item = item.split("\n")
+        for word in split_item:
+            words_in_text.append(word)
+    for i in range(0,len(words_in_text)):
+        checked_word = words_in_text[i].lower()
+        for x in punctuation:
+            checked_word = checked_word.strip(x)
+        if checked_word in list_of_censors:
+            # Censoring the targeted word
+            word_clean = words_in_text[i]
+            censored_word = ""
+            for x in punctuation:
+                word_clean = word_clean.strip(x)
+            for x in range(0,len(word_clean)):
+                censored_word = censored_word + "X"
+            words_in_text[i] = words_in_text[i].replace(word_clean, censored_word)
 
-        
+            # Censoring the word before the targeted word
+            word_before = words_in_text[i-1]
+            for x in punctuation:
+                word_before = word_before.strip(x)
+            censored_word_before = ""
+            for x in range(0,len(word_before)):
+                censored_word_before = censored_word_before + "X"
+            words_in_text[i-1] = words_in_text[i-1].replace(word_before, censored_word_before)
+
+            # Censoring the word after the targeted word
+            word_after = words_in_text[i+1]
+            for x in punctuation:
+                word_after = word_after.strip(x)
+            censored_word_after = ""
+            for x in range(0,len(word_after)):
+                censored_word_after = censored_word_after + "X"
+            words_in_text[i+1] = words_in_text[i+1].replace(word_after, censored_word_after)
+    return " ".join(words_in_text)   
+
 print("FIRST EXERCISE:\n", censor_phrase(email_one, "learning algorithms"))
 print("SECOND EXERCISE:\n", censor_phrase_list(email_two))
 print("THIRD EXERCISE:\n", censor_phrase_list_and_negative_words(email_three))
+print("FOURTH EXERCISE:\n", censor_all_plus_before_after(email_four))
