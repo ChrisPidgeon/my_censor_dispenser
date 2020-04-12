8dadb2e510e7f3a8f4bd8cc18d07d242e69e11e1
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-04-12 17:10:26 +1200

    Completed second exercise, made modifications to first exercise to integrate each function together.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -8,25 +8,42 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
+#This exercise requires me to define a function that can censor a specific word or 
+# phrase from a body of text, and then return the text.
+def censor_phrase(text, to_censor):
+    to_censor_all_cases = []
+    to_censor_all_cases.append(to_censor)
+    to_censor_all_cases.append(to_censor.upper())
+    to_censor_all_cases.append(to_censor.lower())
+    to_censor_all_cases.append(to_censor.title())
 
-#This exercise requires me to define a function 
-#that can censor a specific word or phrase from a body of text, and then return the text.
-def exercise_one(text, to_censor):
-    censor = ""
-    for x in range(0, len(to_censor)):
-        if to_censor[x] == " ":
-            censor += " "
-        else:
-            censor += "!"
-    return text.replace(to_censor, censor)
+    text_clean = text
+    for item in to_censor_all_cases:
+        censor = ""
+        for x in range(0, len(item)):
+            if to_censor[x] == " ":
+                censor += " "
+            else:
+                censor += "!"
+        text_clean = text_clean.replace(item, censor)
+    return text_clean
 
-print(exercise_one(email_one, "learning algorithms"))
-
-def exercise_two():
-    pass
+#This exercises requires me to write a function that can censor not just a specific word or 
+#phrase from a body of text, but a whole list of words and phrases, and then return the text.
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+def censor_phrase_list(text, censor_list):
+    text_clean = text
+    for word in censor_list:
+        text_clean = censor_phrase(text_clean, word)
+    return text_clean
 
 def exercise_three():
     pass
 
 def exercise_four():
     pass
+
+        
+#print(censor_phrase(email_one, "learning algorithms"))
+
+print(censor_phrase_list(email_two, proprietary_terms))
