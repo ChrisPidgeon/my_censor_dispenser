48607e59beffaaaeac27574d92228690e03f70dd
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-04-12 17:28:21 +1200

    Commenting and formatting.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -7,9 +7,11 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
-#This exercise requires me to define a function that can censor a specific word or 
-# phrase from a body of text, and then return the text.
+#This exercise required me to define a function that can censor a specific word or 
+#phrase from a body of text, and then return the text.
 def censor_phrase(text, to_censor):
     to_censor_all_cases = []
     to_censor_all_cases.append(to_censor)
@@ -28,16 +30,21 @@ def censor_phrase(text, to_censor):
         text_clean = text_clean.replace(item, censor)
     return text_clean
 
-#This exercises requires me to write a function that can censor not just a specific word or 
+#This exercise required me to write a function that can censor not just a specific word or 
 #phrase from a body of text, but a whole list of words and phrases, and then return the text.
-proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 def censor_phrase_list(text, censor_list):
     text_clean = text
     for word in censor_list:
         text_clean = censor_phrase(text_clean, word)
     return text_clean
 
-def exercise_three():
+#This exercise required me to write a function that can censor any occurance of a word from the 
+#“negative words” list after any “negative” word has occurred twice, as well as censoring everything 
+#from the list from the previous step as well and use it to censor email_three. 
+#Originally I planned to have each function linked together to create an integrated series
+#of function calls; however, with this function, it became too complicated to manage, 
+#so I shifted to completing this and the following exercise internally. 
+def censor_phrase_list_and_negative_words(text, censor_list, negative_words_list):
     pass
 
 def exercise_four():
